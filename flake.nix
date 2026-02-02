5675ca882b7e7f7a5bb93994fdc18d970e9ef0bc
Hong Jiang <j@1byte.io>
2026-02-03 01:03:27 +0800

    add home-manager

--- flake.nix
+++ flake.nix
@@ -5,15 +5,28 @@
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, zen-browser }: {
+  outputs = { self, nixpkgs, zen-browser, home-manager }: {
     nixosConfigurations.hestia = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
             ./modules/base.nix
             ./hosts/hestia/configuration.nix
-          ];
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.hjiang = ./home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+      ];
       specialArgs = { inherit zen-browser; };
     };
   };
