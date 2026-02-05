57719c8fe1352aaf1f114be22b8a11318d491ff1
Hong Jiang <j@1byte.io>
2026-02-06 00:52:57 +0800

    initegrate foot config

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -73,7 +73,6 @@ in
         "hypridle"
         "hyprpaper"
         "waybar"
-        "foot --server"
         "xhost si:localuser:root"
         "swaync"
         "nm-applet"
@@ -1023,5 +1022,58 @@ in
           background-color: #0069d4;
       }
     '';
+  };
+
+  # Foot terminal configuration
+  programs.foot = {
+    enable = true;
+    server.enable = true;
+
+    settings = {
+      main = {
+        font = "FiraCode Nerd Font:size=12:style=Light";
+      };
+
+      colors = {
+        # Custom background override
+        background = "202020";
+
+        # Catppuccin Mocha theme
+        cursor = "11111b f5e0dc";
+        foreground = "cdd6f4";
+
+        # Normal colors
+        regular0 = "45475a";
+        regular1 = "f38ba8";
+        regular2 = "a6e3a1";
+        regular3 = "f9e2af";
+        regular4 = "89b4fa";
+        regular5 = "f5c2e7";
+        regular6 = "94e2d5";
+        regular7 = "bac2de";
+
+        # Bright colors
+        bright0 = "585b70";
+        bright1 = "f38ba8";
+        bright2 = "a6e3a1";
+        bright3 = "f9e2af";
+        bright4 = "89b4fa";
+        bright5 = "f5c2e7";
+        bright6 = "94e2d5";
+        bright7 = "a6adc8";
+
+        # Extended palette
+        "16" = "fab387";
+        "17" = "f5e0dc";
+
+        # Selection and search
+        selection-foreground = "cdd6f4";
+        selection-background = "414356";
+        search-box-no-match = "11111b f38ba8";
+        search-box-match = "cdd6f4 313244";
+        jump-labels = "11111b fab387";
+        urls = "89b4fa";
+      };
+    };
   };
 }
