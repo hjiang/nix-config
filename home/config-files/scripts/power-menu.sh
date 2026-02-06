c8d9b5f28a48050b009315536a4089a72e520eb6
Hong Jiang <hjiang@users.noreply.github.com>
2026-02-06 15:20:19 +0800

    remove config files to separate files

---
+++ home/config-files/scripts/power-menu.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Power menu options
+options="Lock Screen
+Logout
+Suspend
+Shutdown
+Reboot"
+
+# Show menu and get selection
+selected=$(printf "%s" "$options" | walker --dmenu)
+
+# Execute action based on selection
+case "$selected" in
+    "Lock Screen")
+        loginctl lock-session
+        ;;
+    "Logout")
+        hyprctl dispatch exit
+        ;;
+    "Suspend")
+        loginctl suspend
+        ;;
+    "Shutdown")
+        loginctl poweroff
+        ;;
+    "Reboot")
+        loginctl reboot
+        ;;
+esac
